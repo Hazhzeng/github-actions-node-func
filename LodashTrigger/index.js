ac6e9db1009c91048e0e5b44cf6a60bf3fe93362
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-10-02 13:58:59 -0700

    Try Again

--- LodashTrigger/index.js
+++ LodashTrigger/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     var _ = require('lodash');
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "Hello " + String(_.random(true))
+        body: "Hello " + String(_.random(true)) + " Awesome!"
     };
 };
