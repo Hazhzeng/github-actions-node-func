6d5c33c0396d9ba6b34a12c2f186edd8fde56eed
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-10-02 17:30:12 -0700

    Change testing scenario back to v1

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
     context.res = {
-        body: "Publish by dependency far out you this little good good good"
+        body: "IOPIMAIOBI09123-049F-0OPDIMV093M5F-"
     };
 };
