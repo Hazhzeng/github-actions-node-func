29655540e409841130f288dab1d15c31602389af
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-11-01 11:01:47 -0700

    Check again

--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -3,7 +3,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.res = {
         status: 200,
-        body: "ASDF"
+        body: "ASDFFF"
     };
 };
 
